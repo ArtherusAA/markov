e256f7a109e920bd49bb37ec3df3e192e68b813d
ArtherusAA <artherus@yandex.ru>
2019-09-16 15:58:01 +0300

    Basics

--- main.cpp
+++ main.cpp
@@ -8,25 +8,28 @@ void print_help() {
     printf("This is supposed to be help output\n");
 }
 
-void print_error_message(char *message) {
+void print_error_message(const char *message) {
     printf("error: %s\n", message);
 }
 
 int main(int argc, char** argv) {
     auto inputfile = stdin;
-    if (argc > 0) {
-        if (strcmp(argv[0], "-h") == 0) {
+    bool is_stdin = true;
+    if (argc > 1) {
+        if (strcmp(argv[1], "-h") == 0) {
             print_help();
             return 0;
         } else {
-            FILE *f = open(argv[0], "r");
+            FILE *f = fopen(argv[1], "r");
             if (f == NULL) {
                 print_error_message("no such file");
                 return 0;
             }
             inputfile = f;
+            is_stdin = false;
         }
     }
-    fclose(f);
+    if (!is_stdin)
+        fclose(inputfile);
     return 0;
 }
